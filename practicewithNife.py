14c1f7545bab077423a3f9d35e0c1dd24e9b0939
t-for-tomi <omotoyosiadesina13@gmail.com>
2026-09-23 20:59:42 +0100

    Numerical value change
    I changed the last 2 lines values,5 to 6

--- practicewithNife.py
+++ practicewithNife.py
@@ -1,4 +1,4 @@
-def add(a, b):
+add(a, b):
     return a + b
 
 def subtract(a, b):
@@ -15,5 +15,5 @@ def divide(a, b):
 if __name__ == "__main__":
     print("2 + 3 =", add(2, 3))
     print("10 - 4 =", subtract(10, 4))
-    print("6 * 7 =", multiply(6, 7))
-    print("20 / 5 =", divide(20, 5))
+    print("6 * 7 =", multiply(7, 2))
+    print("20 / 5 =", divide(21, 6))
